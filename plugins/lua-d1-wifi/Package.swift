20facffd77356abc7dfae230f1dd51433442bd83
Lua Design <luadesign.vn@gmail.com>
2026-09-25 08:04:30 +0000

    Fix Capacitor iOS SPM plugin product name

--- plugins/lua-d1-wifi/Package.swift
+++ plugins/lua-d1-wifi/Package.swift
@@ -1,11 +1,20 @@
 // swift-tools-version: 5.9
 import PackageDescription
+
 let package = Package(
-  name: "LuaD1Wifi",
+  name: "LuadesignCapacitorD1Wifi",
   platforms: [.iOS(.v15)],
-  products: [.library(name: "LuaD1Wifi", targets: ["LuaD1WifiPlugin"])],
-  dependencies: [.package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")],
-  targets: [.target(name: "LuaD1WifiPlugin", dependencies: [
-    .product(name: "Capacitor", package: "capacitor-swift-pm")
-  ], path: "ios/Sources/LuaD1WifiPlugin")]
+  products: [
+    .library(name: "LuadesignCapacitorD1Wifi", targets: ["LuaD1WifiPlugin"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+  ],
+  targets: [
+    .target(
+      name: "LuaD1WifiPlugin",
+      dependencies: [.product(name: "Capacitor", package: "capacitor-swift-pm")],
+      path: "ios/Sources/LuaD1WifiPlugin"
+    )
+  ]
 )
